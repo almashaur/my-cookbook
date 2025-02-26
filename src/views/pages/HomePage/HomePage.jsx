17036225682adcd939749392cd9ac9c6179b8d20
Ahmed Almashaur <ahmedalmashaur@gmail.com>
2025-02-26 18:44:18 +0300

    fixes

--- src/views/pages/HomePage/HomePage.jsx
+++ src/views/pages/HomePage/HomePage.jsx
@@ -76,8 +76,8 @@ const Dashboard = (props) => {
         <h1 className="display-5 fw-bold">My Cookbook</h1>
         <div className="col-lg-6 mx-auto">
           <p className="lead mb-4">
-            Quickly design and customize responsive mobile-first sites with
-            Bootstrap – the world’s most popular front-end open source toolkit.
+            A place to share and save recipes, learn new cuisines, and connect
+            with others who share your passion for food.
           </p>
           <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
             <Link to="/allrecipes">
