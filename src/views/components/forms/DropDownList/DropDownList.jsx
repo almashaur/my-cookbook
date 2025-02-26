106b11f5230aa0356f5d9d114bf372ff007a5209
Ahmed Almashaur <ahmedalmashaur@gmail.com>
2025-02-26 09:46:40 +0300

    cross checking dropdown with backend

--- src/views/components/forms/DropDownList/DropDownList.jsx
+++ src/views/components/forms/DropDownList/DropDownList.jsx
@@ -1,65 +1,50 @@
-import React, { useState } from "react";
-import { default as ReactSelect, components } from "react-select";
+import React from "react";
+import ReactSelect, { components } from "react-select";
 
-// Custom Option component for checkboxes
-const Option = (props) => {
-  return (
-    <div>
-      <components.Option {...props}>
-        <input
-          type="checkbox"
-          checked={props.isSelected}
-          onChange={() => null} // Prevent checkbox toggle
-        />{" "}
-        <label>{props.label}</label>
-      </components.Option>
-    </div>
-  );
-};
+const Option = (props) => (
+  <div>
+    <components.Option {...props}>
+      <input type="checkbox" checked={props.isSelected} onChange={() => null} />{" "}
+      <label>{props.label}</label>
+    </components.Option>
+  </div>
+);
 
-// MultiSelect component
-const MultiSelectDropdown = ({ options, onChange, value }) => {
-  return (
-    <ReactSelect
-      options={options}
-      isMulti
-      closeMenuOnSelect={false}
-      hideSelectedOptions={false}
-      components={{ Option }}
-      onChange={onChange}
-      value={value}
-    />
-  );
-};
+const MultiSelectDropdown = ({ options, onChange, value }) => (
+  <ReactSelect
+    options={options}
+    isMulti
+    closeMenuOnSelect={false}
+    hideSelectedOptions={false}
+    components={{ Option }}
+    onChange={onChange}
+    value={value}
+  />
+);
 
-// Example usage in a form (without returning JSX in this snippet)
-const DropDownList = ({selectedOptions, setSelectedOptions}) => {
-//   const [selectedOptions, setSelectedOptions] = useState([]);
-
-  const handleChange = (selectedOptions) => {
-    setSelectedOptions(selectedOptions);
-    console.log(selectedOptions)
+const DropDownList = ({ selectedOptions, setSelectedOptions, onChange }) => {
+  const handleChange = (selected) => {
+    setSelectedOptions(selected);
+    if (onChange) {
+      onChange(selected);
+    }
   };
 
-
   const toolsOptions = [
-    { value: 'Knife', label: 'Knife' },
-    { value: 'Cutting Board', label: 'Cutting Board' },
-    { value: 'Pan', label: 'Pan' },
-    { value: 'Oven', label: 'Oven' },
-    { value: 'Blender', label: 'Blender' },
-    { value: 'Measuring Cup', label: 'Measuring Cup' },
-];
+    { value: "Knife", label: "Knife" },
+    { value: "Cutting Board", label: "Cutting Board" },
+    { value: "Pan", label: "Pan" },
+    { value: "Oven", label: "Oven" },
+    { value: "Blender", label: "Blender" },
+    { value: "Measuring Cup", label: "Measuring Cup" },
+  ];
 
   return (
-    
-      
-      <MultiSelectDropdown
-        options={toolsOptions}
-        onChange={handleChange}
-        value={selectedOptions}
-      />
-      
+    <MultiSelectDropdown
+      options={toolsOptions}
+      onChange={handleChange}
+      value={selectedOptions}
+    />
   );
 };
 
